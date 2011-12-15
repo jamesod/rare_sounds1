b540862e587430e33b6a388f8185127a634cd10c
James O'Driscoll <james.odriscoll14@gmail.com>
2011-12-15 16:02:49 +0000

    Added music listings.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+#release artist genre format date price
+Music.create(:release => 'Could This Be', :artist => 'Noisia', :genre => 'Drum & Bass', :format => 'Vinyl',
+             :date => '2012-11-22', :price => '4.95')
+Music.create(:release => 'Baby I Got It EP', :artist => 'Miguel Campbell', :genre => 'House', :format => 'Vinyl',
+             :date => '2012-12-01', :price => '5.95')
+Music.create(:release => 'Sun City EP', :artist => 'Nick Curly', :genre => 'Tech House', :format => 'CD',
+             :date => '2012-09-26', :price => '7.95')
