c140980743b5819dfd146cdb5413b66483e4f2b2
James O'Driscoll <james.odriscoll14@gmail.com>
2011-12-16 14:29:38 +0000

    Added basic menu items.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RareSounds1::Application.routes.draw do
+  get "welcome/home"
+  get "welcome/sign_up"
+
   resources :musics
 
   # The priority is based upon order of creation:
@@ -50,7 +53,7 @@ RareSounds1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+    root :to => "welcome#home"
 
   # See how all your routes lay out with "rake routes"
 
