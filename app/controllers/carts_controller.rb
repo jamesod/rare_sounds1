4ba9f6030257d2842af74a75fd49d63cf2c23af9
Patrick Doorly <patrickhdoorly@hotmail.com>
2012-01-02 18:20:50 +0000

    added sanitize script if wanted to add links from artists to artist website

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -78,9 +78,10 @@ class CartsController < ApplicationController
   def destroy
     @cart = Cart.find(params[:id])
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to(carts_url) }
+      format.html { redirect_to(musics_url, :notice => 'Your cart is currently empty') }
       format.xml  { head :ok }
     end
   end
--- app/models/music.rb
+++ app/models/music.rb
@@ -2,6 +2,8 @@ class Music < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
 
+   attr_accessible :release, :artist, :genre, :format, :date, :price
+
   def self.search(search_query)
     if search_query
       find(:all, :conditions => ['release || artist LIKE ?', "%#{search_query}%"])
